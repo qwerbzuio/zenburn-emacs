b8cd809d945c1d3879e796baab30e386e55d5600
Benjamin Motz <benjamin.motz@mailbox.org>
2018-10-30 17:06:11 +0100

    Add faces for git-gutter+

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -661,6 +661,13 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(git-gutter-fr:added ((t (:foreground ,zenburn-green  :weight bold))))
    `(git-gutter-fr:deleted ((t (:foreground ,zenburn-red :weight bold))))
    `(git-gutter-fr:modified ((t (:foreground ,zenburn-magenta :weight bold))))
+;;;;; git-gutter+
+   `(git-gutter+-added ((t (:foreground ,zenburn-green :weight bold :inverse-video t))))
+   `(git-gutter+-deleted ((t (:foreground ,zenburn-red :weight bold :inverse-video t))))
+   `(git-gutter+-modified ((t (:foreground ,zenburn-magenta :weight bold :inverse-video t))))
+   `(git-gutter+-unchanged ((t (:foreground ,zenburn-fg :weight bold :inverse-video t))))
+   `(git-gutter+-separator ((t (:foreground ,zenburn-green :weight bold :inverse-video t))))
+   `(git-gutter+-commit-header-face ((t (:foreground ,zenburn-green :weight bold :inverse-video t))))
 ;;;;; git-rebase
    `(git-rebase-hash ((t (:foreground, zenburn-orange))))
 ;;;;; gnus
